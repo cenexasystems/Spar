98fb3d0113b67534172242246809522aaf6c490e
cenexasystems <cenexasystems@gmail.com>
2026-04-03 07:07:28 +0530

    Updated hero section and space background styles

--- src/components/SpaceBackground.jsx
+++ src/components/SpaceBackground.jsx
@@ -10,34 +10,12 @@ const SpaceBackground = () => {
       <div className="glow-orb orb-4"></div>
       <div className="bg-grid-overlay"></div>
 
-      {/* Space Battle and roaming characters */}
-      <div className="bg-rocket-battle">
-        <div className="mini-rocket">
-          <div className="mini-rocket-window">
-            <div className="mini-window-battle">
-              <span className="mini-buzz">🧑‍🚀</span>
-              <span className="mini-vs">⚡</span>
-              <span className="mini-enemy">👾</span>
-            </div>
-          </div>
-          <div className="mini-rocket-flame">🔥</div>
-        </div>
-
-        {/* Roaming mini characters */}
-        <div className="mini-roamer r-buzz">🧑‍🚀</div>
-        <div className="mini-roamer r-alien-1">👽</div>
-        <div className="mini-roamer r-ufo">🛸</div>
-        <div className="mini-roamer r-star-1">⭐</div>
-        <div className="mini-roamer r-star-2">🌟</div>
-        <div className="mini-roamer r-rocket">🚀</div>
-      </div>
-
-      {/* Shooting stars and extra stars */}
+      {/* 3 Shooting Stars (rendered behind content) */}
       <div className="page-shooting-star shooting-star-1"></div>
       <div className="page-shooting-star shooting-star-2"></div>
-      {[...Array(26)].map((_, i) => (
-        <div key={`bg-s-${i}`} className={`bg-page-star star-${i}`}></div>
-      ))}
+      <div className="page-shooting-star shooting-star-3"></div>
+      
+      {/* Core background texture */}
     </div>
   );
 };
